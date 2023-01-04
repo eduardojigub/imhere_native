25e956df0e07d597bf586db3515ef50fca135c36
eduardojigub <eduardobrunoit@gmail.com>
2023-01-04 12:08:31 -0300

    feat: creating first screen

--- App.tsx
+++ App.tsx
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.eventName}>Nome do Evento</Text>
+      <Text style={styles.eventDate}>Sexta-feira, 4 de janeiro de 2023</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -12,9 +13,18 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1, //seta a tela toda para estilização
+    backgroundColor: '#131016',
+    padding: 24,
+  },
+  eventName: {
+    color: '#FDFCFE',
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginTop: 48,
+  },
+  eventDate: {
+    color: '#6B6B6B',
+    fontSize: 16,
   },
 });
